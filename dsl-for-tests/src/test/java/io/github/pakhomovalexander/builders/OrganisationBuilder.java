0007a209d5a6ac6d403f7cdae96bd91ec4d6580d
Pakhomov Aleksandr <Aleksandr.Pakhomv@sbb.ch>
2020-06-28 16:52:49 +0300

    Move build method implementation to CustomerBuilder

--- dsl-for-tests/src/test/java/io/github/pakhomovalexander/builders/OrganisationBuilder.java
+++ dsl-for-tests/src/test/java/io/github/pakhomovalexander/builders/OrganisationBuilder.java
@@ -5,15 +5,14 @@ import io.github.pakhomovalexander.domain.Organisation;
 public class OrganisationBuilder<T extends Organisation> extends CustomerBuilder<T, OrganisationBuilder<T>> {
     private String organisationName;
 
-    public CustomerBuilder<T, OrganisationBuilder<T>> organisationName(String organisationName) {
+    public OrganisationBuilder<T> organisationName(String organisationName) {
         this.organisationName = organisationName;
         return this;
     }
 
     @Override
-    public T build() {
+    protected T createCustomer() {
         T organisation = (T) new Organisation();
-        super.setCustomerFields(organisation);
         organisation.setOrganisationName(organisationName);
 
         return organisation;
